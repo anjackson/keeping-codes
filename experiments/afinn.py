fb8101b8a1272b1243a739d86ad1ed443c2eeca7
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-04-13 12:00:44 +0100

    Added notes.

--- experiments/afinn.py
+++ experiments/afinn.py
@@ -13,13 +13,12 @@
 import math
 import re
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
 
 # AFINN-111 is as of June 2011 the most recent version of AFINN
 filenameAFINN = 'AFINN/AFINN-111.txt'
-afinn = dict(map(lambda (w, s): (w, int(s)), [ 
-            ws.strip().split('\t') for ws in open(filenameAFINN) ]))
+afinn = dict(map(lambda wns: (wns[0], int(wns[1])), [ ws.strip().split('\t') for ws in open(filenameAFINN) ]))
 
 # Word splitter pattern
 pattern_split = re.compile(r"\W+")
@@ -30,11 +29,13 @@ def sentiment(text):
     Positive values are positive valence, negative value are negative valence. 
     """
     words = pattern_split.split(text.lower())
-    sentiments = map(lambda word: afinn.get(word, 0), words)
-    if sentiments:
+    sentiments = []
+    for word in words:
+        sentiments.append(afinn.get(word,0))
+    if len(sentiments) > 0:
+        sentiment = sum(sentiments)/math.sqrt(len(sentiments))
         # How should you weight the individual word sentiments? 
         # You could do N, sqrt(N) or 1 for example. Here I use sqrt(N)
-        sentiment = float(sum(sentiments))/math.sqrt(len(sentiments))
         
     else:
         sentiment = 0
